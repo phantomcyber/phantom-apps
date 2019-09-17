a7c23aae528627130637702e94c818492819414a
matt-phantom <33102061+matt-phantom@users.noreply.github.com>
2019-09-17 12:14:30 -0700

    Merging repo phdarkpoint to github (#20)

---
+++ Apps/phdarkpoint/darkpoint_consts.py
@@ -0,0 +1,9 @@
+# File: darkpoint_consts.py
+# Copyright (c) 2019 Splunk Inc.
+#
+# SPLUNK CONFIDENTIAL - Use or disclosure of this material in whole or in part
+# without a valid written license from Splunk Inc. is PROHIBITED.
+#
+# amount of seconds to sleep in between poll attempts for report status
+DARKPOINT_SLEEP_SECS = 3
+DARKPOINT_MSG_MAX_POLLS_REACHED = 'Reached max polling attempts'
