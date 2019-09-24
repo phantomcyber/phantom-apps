13ef2965e8c6e6808d181d47793d0a13dc1ee5dd
Ricardo Munoz <rmunoz@rmunoz-mbp-9d822.sv.splunk.com>
2019-09-24 16:45:23 -0700

    Merging repo phvictorops to github

---
+++ Apps/phvictorops/victorops_consts.py
@@ -0,0 +1,9 @@
+# File: victorops_consts.py
+# Copyright (c) 2018 Splunk Inc.
+#
+# SPLUNK CONFIDENTIAL - Use or disclosure of this material in whole or in part
+# without a valid written license from Splunk Inc. is PROHIBITED.
+# Define your constants here
+
+INTEGRATION_URL_MISSING = "Integration URL required in asset configuration"
+UPDATE_INCIDENT_ERROR = "Error updating incident"
