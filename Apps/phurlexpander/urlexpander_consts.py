cd2c8765c79312d1190358980402c805fd15ace9
Ricardo Munoz <rmunoz@rmunoz-mbp-9d822.sv.splunk.com>
2019-09-25 16:19:47 -0700

    changed app folder name

---
+++ Apps/phurlexpander/urlexpander_consts.py
@@ -0,0 +1,12 @@
+# --
+# File: urlexpander_consts.py
+# --
+URLEXPANDER_JSON_BITLY_SERVER = "bitly_server"
+URLEXPANDER_JSON_BITLY_APIKEY = "bitly_apikey"
+URLEXPANDER_JSON_GOOGL_SERVER = "googl_server"
+URLEXPANDER_JSON_GOOGL_APIKEY = "googl_apikey"
+
+URLEXPANDER_ERR_SERVER_CONNECTION = "Connection to server failed"
+URLEXPANDER_ERR_CONNECTIVITY_TEST = "Connectivity test failed"
+URLEXPANDER_SUCC_CONNECTIVITY_TEST = "Connectivity test passed"
+URLEXPANDER_URL = "short_url"
