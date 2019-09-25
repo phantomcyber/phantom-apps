2090dcf929edb99841232d6fccfe63fa41d999e4
matt-phantom <33102061+matt-phantom@users.noreply.github.com>
2019-09-25 10:04:00 -0700

    Merge ssmachine into github (#35)
    * Merging repo ssmachine to github
    
    * rename screenshot machine repo to a less questionable directory name

---
+++ Apps/phscreenshotmachine/ssmachine_consts.py
@@ -0,0 +1,7 @@
+# File: ssmachine_consts.py
+# Copyright (c) 2016-2019 Splunk Inc.
+#
+# SPLUNK CONFIDENTIAL - Use or disclosure of this material in whole or in part
+# without a valid written license from Splunk Inc. is PROHIBITED.
+# --
+SSMACHINE_JSON_DOMAIN = "http://api.screenshotmachine.com/"
