e1154592f0f3cca48da5d54f692aebca9923469f
sam-splunk <51926487+sam-splunk@users.noreply.github.com>
2019-09-25 11:47:51 -0600

    Adjust license text in files. (#38)

--- Apps/phredseal/redseal_consts.py
+++ Apps/phredseal/redseal_consts.py
@@ -1,8 +1,8 @@
 # File: redseal_consts.py
 # Copyright (c) 2019 Splunk Inc.
 #
-# SPLUNK CONFIDENTIAL - Use or disclosure of this material in whole or in part
-# without a valid written license from Splunk Inc. is PROHIBITED.
+# Licensed under Apache 2.0 (https://www.apache.org/licenses/LICENSE-2.0.txt)
+#
 
 REDSEAL_CONFIG_SERVER_URL = 'server_url'
 REDSEAL_CONFIG_VERIFY_SERVER_CERT = 'verify_server_cert'
