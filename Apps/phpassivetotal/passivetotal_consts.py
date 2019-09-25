47442f67538b8023b403a3ddbf321ca6650119e9
Ricardo Munoz <rmunoz@rmunoz-mbp-9d822.sv.splunk.com>
2019-09-25 16:37:04 -0700

    Merging repo phpassivetotal to github from source directory passivetotal

---
+++ Apps/phpassivetotal/passivetotal_consts.py
@@ -0,0 +1,41 @@
+# File: passivetotal_consts.py
+# Copyright (c) 2016-2019 Splunk Inc.
+#
+# SPLUNK CONFIDENTIAL - Use or disclosure of this material in whole or in part
+# without a valid written license from Splunk Inc. is PROHIBITED.
+
+
+PASSIVETOTAL_JSON_DOMAIN = "domain"
+PASSIVETOTAL_JSON_IP = "ip"
+PASSIVETOTAL_JSON_KEY = "key"
+PASSIVETOTAL_JSON_SECRET = "secret"
+PASSIVETOTAL_JSON_CLASSIFICATION = "classification"
+PASSIVETOTAL_JSON_EVER_COMPROMISED = "ever_compromised"
+PASSIVETOTAL_JSON_DYNAMIC = "dynamic"
+PASSIVETOTAL_JSON_WATCHING = "watching"
+PASSIVETOTAL_JSON_DYNAMIC_DOMAIN = "dynamic_domain"
+PASSIVETOTAL_JSON_BEING_WATCHED = "being_watched"
+PASSIVETOTAL_JSON_METADATA = "metadata"
+PASSIVETOTAL_JSON_PASSIVE = "passive"
+PASSIVETOTAL_JSON_SINKHOLE = "sinkhole"
+PASSIVETOTAL_JSON_SUBDOMAINS = "subdomains"
+PASSIVETOTAL_JSON_TAGS = "tags"
+PASSIVETOTAL_JSON_UNIQUE = "unique"
+PASSIVETOTAL_JSON_SSL_CERTIFICATES = "ssl_certificates"
+PASSIVETOTAL_JSON_AS_NAME = "as_name"
+PASSIVETOTAL_JSON_COUTRY = "country"
+PASSIVETOTAL_JSON_FIRST_SEEN = "first_seen"
+PASSIVETOTAL_JSON_LAST_SEEN = "last_seen"
+PASSIVETOTAL_JSON_TOTAL_UNIQUE_DOMAINS = "total_unique_domains"
+PASSIVETOTAL_JSON_FROM = "from"
+PASSIVETOTAL_JSON_TO = "to"
+
+PASSIVETOTAL_ERR_CONNECTIVITY_TEST = "Connectivity test failed"
+PASSIVETOTAL_SUCC_CONNECTIVITY_TEST = "Connectivity test passed"
+PASSIVETOTAL_ERR_SERVER_CONNECTION = "Connection failed"
+PASSIVETOTAL_ERR_FROM_SERVER = "API failed, Status code: {status}, Message: {message}"
+PASSIVETOTAL_MSG_GET_DOMAIN_TEST = "Querying a single domain to check credentials"
+
+PASSIVETOTAL_USING_BASE_URL = "Using url: {base_url}"
+
+PASSIVETOTAL_REST_API_URL = "https://api.passivetotal.org/v2"
