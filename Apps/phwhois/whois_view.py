d90f41d6de495f8c1be3037760823fa00c25a7e9
sam-splunk <51926487+sam-splunk@users.noreply.github.com>
2019-09-26 11:42:24 -0600

    Adjust license text on apps. (#68)

--- Apps/phwhois/whois_view.py
+++ Apps/phwhois/whois_view.py
@@ -1,8 +1,8 @@
 # File: whois_view.py
 # Copyright (c) 2014-2019 Splunk Inc.
 #
-# SPLUNK CONFIDENTIAL - Use or disclosure of this material in whole or in part
-# without a valid written license from Splunk Inc. is PROHIBITED.
+# Licensed under Apache 2.0 (https://www.apache.org/licenses/LICENSE-2.0.txt)
+#
 
 
 def get_ctx_result(provides, result):
