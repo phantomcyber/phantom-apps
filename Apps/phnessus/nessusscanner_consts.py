d068641ef743bbee25ca821416eb3d6809412cdd
matt-phantom <33102061+matt-phantom@users.noreply.github.com>
2019-09-26 10:12:52 -0700

    Merge phnessus into github from directory nessus (#62)
    * Merging repo phnessus to github from source directory nessus
    
    * Delete docker-compose.yml

---
+++ Apps/phnessus/nessusscanner_consts.py
@@ -0,0 +1,22 @@
+# --
+# File: nessusscanner_consts.py
+#
+# Copyright (c) Phantom Cyber Corporation, 2018
+#
+# This unpublished material is proprietary to Phantom Cyber.
+# All rights reserved. The methods and
+# techniques described herein are considered trade secrets
+# and/or confidential. Reproduction or distribution, in whole
+# or in part, is forbidden except by express written permission
+# of Phantom Cyber.
+#
+# --
+
+TARGET_TO_SCAN = "target_to_scan"
+POLICY_ID = "policy_id"
+
+NESSUS_SERVER = "nessus_server"
+ACCESS_KEY = "access_key"
+SECRET_KEY = "secret_key"
+VERIFY_CERT = "verify_server_cert"
+LISTEN_PORT = "listen_port"
