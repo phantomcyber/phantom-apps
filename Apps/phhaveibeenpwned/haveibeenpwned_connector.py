e0d46605be502b176b6988c817556b2bc70e1e15
ricardo-phantom <38865072+ricardo-phantom@users.noreply.github.com>
2019-09-30 15:25:32 -0700

    Merge phhaveibeenpwned into github from directory haveibeenpwned (#71)
    * Merging repo phhaveibeenpwned to github from source directory haveibeenpwned
    
    * Delete docker-compose.yml
    
    * Update haveibeenpwned_connector.py
    
    * Update haveibeenpwned_consts.py

---
+++ Apps/phhaveibeenpwned/haveibeenpwned_connector.py
@@ -0,0 +1,167 @@
+# --
+# File: haveibeenpwned_connector.py
+#
+# Copyright (c) Phantom Cyber Corporation, 2016-2019
+#
+# Licensed under Apache 2.0 (https://www.apache.org/licenses/LICENSE-2.0.txt)
+#
+# --
+
+# Phantom imports
+import phantom.app as phantom
+from phantom.base_connector import BaseConnector
+from phantom.action_result import ActionResult
+
+# THIS Connector imports
+from haveibeenpwned_consts import *
+
+import requests
+import simplejson as json
+
+
+class HaveIBeenPwnedConnector(BaseConnector):
+    ACTION_ID_LOOKUP_DOMAIN = "lookup_domain"
+    ACTION_ID_LOOKUP_EMAIL = "lookup_email"
+
+    def __init__(self):
+
+        super(HaveIBeenPwnedConnector, self).__init__()
+
+    def _make_rest_call(self, endpoint, params=None):
+
+        full_url = HAVEIBEENPWNED_API_BASE_URL + endpoint
+
+        try:
+
+            response = requests.get(full_url, params=params)
+
+        except:
+
+            return phantom.APP_ERROR, HAVEIBEENPWNED_REST_CALL_FAILURE
+
+        if response.status_code in HAVEIBEENPWNED_BAD_RESPONSE_CODES:
+
+            if response.status_code == HAVEIBEENPWNED_STATUS_CODE_NO_DATA:
+
+                return phantom.APP_SUCCESS, [{"not_found": HAVEIBEENPWNED_BAD_RESPONSE_CODES[response.status_code]}]
+
+            return phantom.APP_ERROR, HAVEIBEENPWNED_BAD_RESPONSE_CODES[response.status_code]
+
+        try:
+
+            resp_json = response.json()
+
+        except:
+
+            return phantom.APP_ERROR, HAVEIBEENPWNED_REST_CALL_JSON_FAILURE
+
+        return phantom.APP_SUCCESS, resp_json
+
+    def _lookup_domain(self, params):
+
+        action_result = self.add_action_result(ActionResult(dict(params)))
+
+        domain = params[HAVEIBEENPWNED_ACTION_PARAM_DOMAIN]
+
+        if phantom.is_url(domain):
+            domain = phantom.get_host_from_url(domain).replace("www.", "")
+
+        if "www." in domain:
+            domain = domain.replace("www.", "")
+
+        endpoint = HAVEIBEENPWNED_API_ENDPOINT_LOOKUP_DOMAIN
+
+        kwargs = {HAVEIBEENPWEND_PARAM_DOMAIN_KEY: domain}
+
+        ret_val, response = self._make_rest_call(endpoint, params=kwargs)
+
+        if (phantom.is_fail(ret_val)):
+
+            return action_result.set_status(phantom.APP_ERROR, HAVEIBEENPWNED_REST_CALL_ERR, response)
+
+        for item in response:
+
+            action_result.add_data(item)
+
+        action_result.set_summary({HAVEIBEENPWNED_TOTAL_BREACHES: len(response)})
+
+        return action_result.set_status(phantom.APP_SUCCESS, HAVEIBEENPWNED_LOOKUP_DOMAIN_SUCCESS)
+
+    def _lookup_email(self, params):
+
+        action_result = self.add_action_result(ActionResult(dict(params)))
+
+        email = params[HAVEIBEENPWNED_ACTION_PARAM_EMAIL]
+
+        endpoint = HAVEIBEENPWNED_API_ENDPOINT_LOOKUP_EMAIL.format(email=email)
+
+        ret_val, response = self._make_rest_call(endpoint)
+
+        if (phantom.is_fail(ret_val)):
+
+            return action_result.set_status(phantom.APP_ERROR, HAVEIBEENPWNED_REST_CALL_ERR, response)
+
+        for item in response:  # Response ends up being a list
+
+            action_result.add_data(item)
+
+        if "not_found" in response[0]:
+
+            action_result.set_summary({HAVEIBEENPWNED_TOTAL_BREACHES: 0})
+
+        else:
+
+            action_result.set_summary({HAVEIBEENPWNED_TOTAL_BREACHES: len(response)})
+
+        return action_result.set_status(phantom.APP_SUCCESS, HAVEIBEENPWNED_LOOKUP_EMAIL_SUCCESS)
+
+    def handle_action(self, params):
+
+        action = self.get_action_identifier()
+
+        ret_val = phantom.APP_SUCCESS
+
+        if (action == self.ACTION_ID_LOOKUP_DOMAIN):
+
+            ret_val = self._lookup_domain(params)
+
+        elif (action == self.ACTION_ID_LOOKUP_EMAIL):
+
+            ret_val = self._lookup_email(params)
+
+        return ret_val
+
+
+if __name__ == '__main__':
+    """ Code that is executed when run in standalone debug mode
+    for .e.g:
+    python2.7 ./zendesk_connector.py /tmp/zendesk_test_create_ticket.json
+        """
+
+    # Imports
+    import sys
+    import pudb
+
+    # Breakpoint at runtime
+    pudb.set_trace()
+
+    # The first param is the input json file
+    with open(sys.argv[1]) as f:
+        # Load the input json file
+        in_json = f.read()
+        in_json = json.loads(in_json)
+        print(json.dumps(in_json, indent=' ' * 4))
+
+        # Create the connector class object
+        connector = HaveIBeenPwnedConnector()
+
+        # Se the member vars
+        connector.print_progress_message = True
+
+        # Call BaseConnector::_handle_action(...) to kickoff action handling.
+        ret_val = connector._handle_action(json.dumps(in_json), None)
+
+        # Dump the return value
+        print ret_val
+
+    exit(0)
---
+++ Apps/phhaveibeenpwned/haveibeenpwned_consts.py
@@ -0,0 +1,26 @@
+# --
+# File: haveibeenpwned_consts.py
+#
+# Copyright (c) Phantom Cyber Corporation, 2016-2019
+#
+# Licensed under Apache 2.0 (https://www.apache.org/licenses/LICENSE-2.0.txt)
+#
+# --
+
+HAVEIBEENPWNED_API_BASE_URL = "https://haveibeenpwned.com/api/v2/"
+HAVEIBEENPWNED_API_ENDPOINT_LOOKUP_EMAIL = "breachedaccount/{email}"
+HAVEIBEENPWNED_API_ENDPOINT_LOOKUP_DOMAIN = "breaches"
+HAVEIBEENPWNED_ACTION_PARAM_EMAIL = "email"
+HAVEIBEENPWNED_ACTION_PARAM_DOMAIN = "domain"
+HAVEIBEENPWEND_PARAM_DOMAIN_KEY = "domain"
+HAVEIBEENPWNED_REST_CALL_ERR = "Error during REST call occurred"
+HAVEIBEENPWNED_LOOKUP_DOMAIN_SUCCESS = "Lookup Domain succeeded"
+HAVEIBEENPWNED_LOOKUP_EMAIL_SUCCESS = "Lookup Email succeeded"
+HAVEIBEENPWNED_REST_CALL_FAILURE = "REST call failed"
+HAVEIBEENPWNED_REST_CALL_JSON_FAILURE = "Conversion of Response to JSON failed"
+HAVEIBEENPWNED_BAD_RESPONSE_CODES = {400: "Bad Request - the account does not comply with an acceptable format",
+                                     403: "Forbidden - no user agent has been specified in the request",
+                                     404: "Not found - The account could not be found and has likely not been pwned",
+                                     429: "Too many requests - the rate limit has been exceeded"}
+HAVEIBEENPWNED_STATUS_CODE_NO_DATA = 404
+HAVEIBEENPWNED_TOTAL_BREACHES = "total_breaches"
