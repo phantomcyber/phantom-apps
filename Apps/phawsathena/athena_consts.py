6f89e9d0299e27988d3048e0e603e19e0230478b
ricardo-phantom <38865072+ricardo-phantom@users.noreply.github.com>
2019-10-04 12:47:39 -0700

    Merge phawsathena into github from directory awsathena (#86)
    * Merging repo phawsathena to github from source directory awsathena
    
    * cleaned repo and added wheels:
    
    * small changes to json

---
+++ Apps/phawsathena/athena_consts.py
@@ -0,0 +1,7 @@
+# File: athena_consts.py
+# Copyright (c) 2017-2019 Splunk Inc.
+#
+# Licensed under Apache 2.0 (https://www.apache.org/licenses/LICENSE-2.0.txt)
+
+ATHENA_JSON_ACCESS_KEY = 'access_key'
+ATHENA_JSON_SECRET_KEY = 'secret_key'
