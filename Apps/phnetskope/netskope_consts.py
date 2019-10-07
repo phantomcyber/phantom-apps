a5b7a2d490a45db7e3623e8adab21a6201198d56
Ricardo Munoz <rmunoz@rmunoz-mbp-9d822.sv.splunk.com>
2019-10-07 12:18:35 -0700

    fixed copyrights

--- Apps/phnetskope/netskope_consts.py
+++ Apps/phnetskope/netskope_consts.py
@@ -1,8 +1,7 @@
 # File: netskope_consts.py
 # Copyright (c) 2018-2019 Splunk Inc.
 #
-# SPLUNK CONFIDENTIAL - Use or disclosure of this material in whole or in part
-# without a valid written license from Splunk Inc. is PROHIBITED.
+# Licensed under Apache 2.0 (https://www.apache.org/licenses/LICENSE-2.0.txt)
 
 NETSKOPE_CONFIG_SERVER_URL = 'server_url'
 NETSKOPE_CONFIG_API_KEY = 'api_key'
