32b28642cc657f047a459582b873355387419e6b
Ricardo Munoz <rmunoz@rmunoz-mbp-9d822.sv.splunk.com>
2019-10-07 12:41:15 -0700

    Merging repo phokta to github from source directory phokta

---
+++ Apps/phokta/okta_view.py
@@ -0,0 +1,125 @@
+# --
+# File: okta_view.py
+#
+# Copyright (c) 2018-2019 Splunk Inc.
+
+# SPLUNK CONFIDENTIAL - Use or disclosure of this material in whole or in part
+# without a valid written license from Splunk Inc. is PROHIBITED.
+#
+# --
+
+
+def get_ctx_result(result):
+
+    ctx_result = {}
+    param = result.get_param()
+    summary = result.get_summary()
+    data = result.get_data()
+
+    ctx_result['param'] = param
+
+    if (data):
+        ctx_result['data'] = data
+
+    if (summary):
+        ctx_result['summary'] = summary
+
+    return ctx_result
+
+
+def display_identity_providers(provides, all_app_runs, context):
+
+    context['results'] = results = []
+    for summary, action_results in all_app_runs:
+        for result in action_results:
+
+            ctx_result = get_ctx_result(result)
+            if (not ctx_result):
+                continue
+            results.append(ctx_result)
+    # print context
+    return 'display_identity_providers.html'
+
+
+def display_add_group(provides, all_app_runs, context):
+
+    context['results'] = results = []
+    for summary, action_results in all_app_runs:
+        for result in action_results:
+
+            ctx_result = get_ctx_result(result)
+            if (not ctx_result):
+                continue
+            results.append(ctx_result)
+    # print context
+    return 'display_add_group.html'
+
+
+def display_get_group(provides, all_app_runs, context):
+
+    context['results'] = results = []
+    for summary, action_results in all_app_runs:
+        for result in action_results:
+
+            ctx_result = get_ctx_result(result)
+            if (not ctx_result):
+                continue
+            results.append(ctx_result)
+    # print context
+    return 'display_get_group.html'
+
+
+def display_set_password(provides, all_app_runs, context):
+
+    context['results'] = results = []
+    for summary, action_results in all_app_runs:
+        for result in action_results:
+
+            ctx_result = get_ctx_result(result)
+            if (not ctx_result):
+                continue
+            results.append(ctx_result)
+    # print context
+    return 'display_set_password.html'
+
+
+def display_get_user(provides, all_app_runs, context):
+
+    context['results'] = results = []
+    for summary, action_results in all_app_runs:
+        for result in action_results:
+
+            ctx_result = get_ctx_result(result)
+            if (not ctx_result):
+                continue
+            results.append(ctx_result)
+    # print context
+    return 'display_get_user.html'
+
+
+def display_list_user_groups(provides, all_app_runs, context):
+
+    context['results'] = results = []
+    for summary, action_results in all_app_runs:
+        for result in action_results:
+
+            ctx_result = get_ctx_result(result)
+            if (not ctx_result):
+                continue
+            results.append(ctx_result)
+    # print context
+    return 'display_list_user_groups.html'
+
+
+def display_list_user(provides, all_app_runs, context):
+
+    context['results'] = results = []
+    for summary, action_results in all_app_runs:
+        for result in action_results:
+
+            ctx_result = get_ctx_result(result)
+            if (not ctx_result):
+                continue
+            results.append(ctx_result)
+    # print context
+    return 'display_list_user.html'
