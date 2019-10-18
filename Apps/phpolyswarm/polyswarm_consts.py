593df7854483266e3bd829abd364b80ed6d8b7e5
jpatel <jpatel@splunk.com>
2019-10-18 16:48:26 +0530

    PolySwarm: All bugs fixation as a community app release on the Open SOurce platform of phantom-apps

--- Apps/phpolyswarm/polyswarm_consts.py
+++ Apps/phpolyswarm/polyswarm_consts.py
@@ -1,1 +1,9 @@
+# File: polyswarm_consts.py
+# Copyright (c) 2019 Splunk Inc.
+#
+# Licensed under Apache 2.0 (https://www.apache.org/licenses/LICENSE-2.0.txt)
+
 POLYSWARM_URL_RESULTS = 'https://polyswarm.network/scan/results'
+POLYSWARM_CONFIG_PARAMS_ERR_MSG = "Please check your asset configuration parameters."
+POLYSWARM_DEBUG_ERROR_MSG = "Error occurred while logging the details of the 'polyswarm_community' and 'base_url' details in the debug spawn logs. "
+POLYSWARM_DEBUG_ERROR_MSG += POLYSWARM_CONFIG_PARAMS_ERR_MSG
