75cb72387e71e39720b8dc9fd44b86f2303eb957
mjiyani <mjiyani@splunk.com>
2019-11-20 14:15:40 +0530

    SLashnext : initial code submission and all bugs fixation

---
+++ Apps/phslashnextphishingincidentresponse/slashnextphishingincidentresponse_consts.py
@@ -0,0 +1,22 @@
+# File: slashnextphishingincidentresponse_consts.py
+# Copyright (c) 2019 SlashNext Inc. (www.slashnext.com)
+#
+# Licensed under Apache 2.0 (https://www.apache.org/licenses/LICENSE-2.0.txt)
+#
+
+"""
+Created on August 20, 2019
+
+@author: Saadat Abid, Umair Ahmad
+"""
+
+# Define your constants here
+BASE_API = 'https://oti.slashnext.cloud/api'
+HOST_REPUTE_API = '/oti/v1/host/reputation'
+URL_REPUTE_API = '/oti/v1/url/reputation'
+URL_SCAN_API = '/oti/v1/url/scan'
+URL_SCANSYNC_API = '/oti/v1/url/scansync'
+HOST_REPORT_API = '/oti/v1/host/report'
+DL_SC_API = '/oti/v1/download/screenshot'
+DL_HTML_API = '/oti/v1/download/html'
+DL_TEXT_API = '/oti/v1/download/text'
