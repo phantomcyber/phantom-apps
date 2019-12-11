e6ba00a3316cb4d4e8b5aacc8e23124c3de6ec5d
Ricardo Munoz <rmunoz@rmunoz-mbp-9d822.sv.splunk.com>
2019-12-11 14:09:38 -0800

    update copyrights

--- Apps/phcrowdstrikeoauth/crowdstrikeoauthapi_consts.py
+++ Apps/phcrowdstrikeoauth/crowdstrikeoauthapi_consts.py
@@ -1,8 +1,7 @@
 # File: crowdstrikeoauthapi_consts.py
 # Copyright (c) 2019 Splunk Inc.
 #
-# SPLUNK CONFIDENTIAL - Use or disclosure of this material in whole or in part
-# without a valid written license from Splunk Inc. is PROHIBITED.
+# Licensed under Apache 2.0 (https://www.apache.org/licenses/LICENSE-2.0.txt)
 
 # Json keys specific to the app's input parameters/config and the output result
 CROWDSTRIKE_JSON_URL_OAuth = "url"
