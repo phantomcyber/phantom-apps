1b3d2848fbb9059bf7622a6b126582bc293aef0d
Ricardo Munoz <rmunoz@rmunoz-mbp-9d822.sv.splunk.com>
2019-12-13 13:39:16 -0800

    update cr

--- Apps/phfortigate/__init__.py
+++ Apps/phfortigate/__init__.py
@@ -1,13 +1,5 @@
-# --
-# File: fortigate/__init__.py
+# File: __init__.py
+# Copyright (c) 2016-2019 Splunk Inc.
 #
-# Copyright (c) Phantom Cyber Corporation, 2016-2018
-#
-# This unpublished material is proprietary to Phantom Cyber.
-# All rights reserved. The methods and
-# techniques described herein are considered trade secrets
-# and/or confidential. Reproduction or distribution, in whole
-# or in part, is forbidden except by express written permission
-# of Phantom Cyber Corporation.
-#
+# Licensed under Apache 2.0 (https://www.apache.org/licenses/LICENSE-2.0.txt)
 # --
--- Apps/phfortigate/fortigate_connector.py
+++ Apps/phfortigate/fortigate_connector.py
@@ -1,14 +1,7 @@
-# --
-# File: fortigate/fortigate_connector.py
+# File: fortigate_connector.py
+# Copyright (c) 2016-2019 Splunk Inc.
 #
-# Copyright (c) Phantom Cyber Corporation, 2016-2018
-#
-# This unpublished material is proprietary to Phantom Cyber.
-# All rights reserved. The methods and
-# techniques described herein are considered trade secrets
-# and/or confidential. Reproduction or distribution, in whole
-# or in part, is forbidden except by express written permission
-# of Phantom Cyber Corporation.
+# Licensed under Apache 2.0 (https://www.apache.org/licenses/LICENSE-2.0.txt)
 #
 # --
 
