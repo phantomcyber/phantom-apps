1a9f6ee0185298228fef3070afd6e7204648d12e
Drouin, Robert <Robert.Drouin@worldpay.com>
2020-01-08 14:35:09 -0500

    Revert "Merge branch 'master' of https://github.com/phantomcyber/phantom-apps.git"
    This reverts commit 3975da2c6f2a68903c2f97ad597677c1d85e9acf, reversing
    changes made to a87dac73411b2f0d5dc1f8d46408f40aea9600a9.

--- Apps/phslashnextphishingincidentresponse/__init__.py
+++
@@ -1,4 +0,0 @@
-# File: __init__.py
-# Copyright (c) 2019 SlashNext Inc. (www.slashnext.com)
-#
-# Licensed under Apache 2.0 (https://www.apache.org/licenses/LICENSE-2.0.txt)
--- Apps/phtwitter/__init__.py
+++
@@ -1,4 +0,0 @@
-# File: __init__.py
-# Copyright (c) 2019 Splunk Inc.
-#
-# Licensed under Apache 2.0 (https://www.apache.org/licenses/LICENSE-2.0.txt)
