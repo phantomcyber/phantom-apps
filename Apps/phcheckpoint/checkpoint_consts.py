bb0e79c49fda9f77da2f86f0d3e0a2d373da03c6
Ricardo Munoz <rmunoz@rmunoz-mbp-9d822.sv.splunk.com>
2020-01-22 22:29:37 -0800

    Merging repo phcheckpoint to github from source directory checkpoint

---
+++ Apps/phcheckpoint/checkpoint_consts.py
@@ -0,0 +1,22 @@
+# --
+# File: checkpoint_consts.py
+#
+# Copyright (c) Phantom Cyber Corporation, 2017
+#
+# This unpublished material is proprietary to Phantom Cyber.
+# All rights reserved. The methods and
+# techniques described herein are considered trade secrets
+# and/or confidential. Reproduction or distribution, in whole
+# or in part, is forbidden except by express written permission
+# of Phantom Cyber.
+#
+# --
+
+CHECKPOINT_ERR_DEVICE_CONNECTIVITY = "Could not connect to Check Point:\n{0}"
+CHECKPOINT_ERR_DEVICE_CONNECTIVITY_NOFORMAT = "Could not connect to Check Point"
+CHECKPOINT_ERR_CONNECTIVITY_TEST = "Connectivity test failed"
+CHECKPOINT_SUCC_CONNECTIVITY_TEST = "Connectivity test passed"
+
+CHECKPOINT_PROG_USING_BASE_URL = "Using base URL '{base_url}'"
+
+CHECKPOINT_OBJECT_NAME_PREFIX = "Phantom - {0}"
