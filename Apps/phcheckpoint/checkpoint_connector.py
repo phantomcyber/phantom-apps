aff8ca6da1b8e5a33f29a3b4acaceb061f4626be
Ricardo Munoz <rmunoz@rmunoz-mbp-9d822.sv.splunk.com>
2020-01-22 22:35:08 -0800

    fixed copyrights, updated min phantom version

--- Apps/phcheckpoint/checkpoint_connector.py
+++ Apps/phcheckpoint/checkpoint_connector.py
@@ -1,16 +1,7 @@
-# --
 # File: checkpoint_connector.py
+# Copyright (c) 2017-2020 Splunk Inc.
 #
-# Copyright (c) Phantom Cyber Corporation, 2017
-#
-# This unpublished material is proprietary to Phantom Cyber.
-# All rights reserved. The methods and
-# techniques described herein are considered trade secrets
-# and/or confidential. Reproduction or distribution, in whole
-# or in part, is forbidden except by express written permission
-# of Phantom Cyber.
-#
-# --
+# Licensed under Apache 2.0 (https://www.apache.org/licenses/LICENSE-2.0.txt)
 
 # Phantom imports
 import phantom.app as phantom
--- Apps/phcheckpoint/checkpoint_consts.py
+++ Apps/phcheckpoint/checkpoint_consts.py
@@ -1,16 +1,7 @@
-# --
 # File: checkpoint_consts.py
+# Copyright (c) 2017-2020 Splunk Inc.
 #
-# Copyright (c) Phantom Cyber Corporation, 2017
-#
-# This unpublished material is proprietary to Phantom Cyber.
-# All rights reserved. The methods and
-# techniques described herein are considered trade secrets
-# and/or confidential. Reproduction or distribution, in whole
-# or in part, is forbidden except by express written permission
-# of Phantom Cyber.
-#
-# --
+# Licensed under Apache 2.0 (https://www.apache.org/licenses/LICENSE-2.0.txt)
 
 CHECKPOINT_ERR_DEVICE_CONNECTIVITY = "Could not connect to Check Point:\n{0}"
 CHECKPOINT_ERR_DEVICE_CONNECTIVITY_NOFORMAT = "Could not connect to Check Point"
