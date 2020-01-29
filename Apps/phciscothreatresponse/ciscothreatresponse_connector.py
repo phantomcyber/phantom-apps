de9fa692287d2741839d4e5a0e5a8ec3b9ac9c35
CAWABANGA <cawabanga2811@gmail.com>
2020-01-29 18:16:35 +0200

    Added sorting on Verdict and Context action. Testing scope for test connectivity is added.

--- Apps/phciscothreatresponse/ciscothreatresponse_connector.py
+++ Apps/phciscothreatresponse/ciscothreatresponse_connector.py
@@ -10,12 +10,14 @@ from phantom.action_result import ActionResult
 # Usage of the consts file is recommended
 # from ciscothreatresponse_consts import *
 from datetime import datetime, timedelta
+import functools
 import json
+import re
 
 import humanize
 import requests
 from collections import Counter
-from requests.exceptions import HTTPError
+from requests.exceptions import HTTPError, ConnectionError
 from threatresponse import ThreatResponse
 from threatresponse.exceptions import RegionError
 
@@ -23,6 +25,19 @@ from threatresponse.exceptions import RegionError
 class RetVal(tuple):
     def __new__(cls, val1, val2=None):
         return tuple.__new__(RetVal, (val1, val2))
+
+
+class ScopeError(Exception):
+    """ Raised when scope check fails. """
+
+    def __init__(self, missing_scopes):
+        scope_names = list({re.findall(r'\w+', scope)[0].capitalize()
+                            for scope in missing_scopes})
+
+        super(ScopeError, self).__init__(
+            'Missing scopes: {}. Please, make sure that your API client has '
+            'correct scope settings.'.format(', '.join(sorted(scope_names)))
+        )
 
 
 class CiscoThreatResponseConnector(BaseConnector):
@@ -45,15 +60,17 @@ class CiscoThreatResponseConnector(BaseConnector):
         self._region = None
         self._tr = None
 
+        self._action_result = None
+
     def _handle_test_connectivity(self, param):
-        action_result = self.add_action_result(ActionResult(dict(param)))
+        self._action_result.set_param(dict(param))
 
         # Connectivity is tested while tr module initialization
         # at initialize method before action handling.
         # If the execution got here, connection is OK.
 
         self.save_progress("Test Connectivity Passed")
-        return action_result.set_status(phantom.APP_SUCCESS)
+        return self._action_result.set_status(phantom.APP_SUCCESS)
 
     def format_time(self, time):
         initial_format = '%Y-%m-%dT%H:%M:%S.%fZ'
@@ -69,7 +86,7 @@ class CiscoThreatResponseConnector(BaseConnector):
         self.save_progress("In action handler for: {0}".format(
             self.get_action_identifier()))
 
-        action_result = self.add_action_result(ActionResult(dict(param)))
+        self._action_result.set_param(dict(param))
 
         observable = param['observable']
 
@@ -92,7 +109,7 @@ class CiscoThreatResponseConnector(BaseConnector):
                         e['module'],
                         self.format_time(doc.get('targets',
                                                  mock_for_no_targets)[0]
-                                            .get('observed_time', {}).get(
+                            .get('observed_time', {}).get(
                             'start_time', '---')),
                         doc.get('targets', mock_for_no_targets)[0].get(
                             'type', '---'),
@@ -130,15 +147,15 @@ class CiscoThreatResponseConnector(BaseConnector):
 
         summary_bar = self.get_summary_bar(observables, contexts)
 
-        action_result.add_data({'response': result,
-                                'context': contexts,
-                                'summary': summary_bar})
+        self._action_result.add_data({'response': result,
+                                      'context': contexts,
+                                      'summary': summary_bar})
 
-        summary = action_result.update_summary({})
+        summary = self._action_result.update_summary({})
         summary['num_data'] = len(contexts)
 
         self.save_progress("Context OK")
-        return action_result.set_status(phantom.APP_SUCCESS)
+        return self._action_result.set_status(phantom.APP_SUCCESS)
 
     def get_summary_bar(self, observables, contexts):
         targets = []
@@ -193,15 +210,15 @@ class CiscoThreatResponseConnector(BaseConnector):
     def _humanize_expiration(self, verdicts):
         return [
             v.update({'expiration':
-                      self._expiration(
-                          v.get('expiration'))}) or v for v in verdicts
+                    self._expiration(
+                            v.get('expiration'))}) or v for v in verdicts
         ]
 
     def _handle_verdict(self, param):
         self.save_progress("In action handler for: {0}".format(
             self.get_action_identifier()))
 
-        action_result = self.add_action_result(ActionResult(dict(param)))
+        self._action_result.set_param(dict(param))
 
         observable = param['observable']
 
@@ -210,14 +227,14 @@ class CiscoThreatResponseConnector(BaseConnector):
 
         verdicts = self._humanize_expiration(result.get('verdicts', []))
 
-        action_result.add_data({'response': result.get('response'),
-                                'verdicts': verdicts})
+        self._action_result.add_data({'response': result.get('response'),
+                                      'verdicts': verdicts})
 
-        summary = action_result.update_summary({})
+        summary = self._action_result.update_summary({})
         summary['num_data'] = len(verdicts)
 
         self.save_progress("Verdicts OK")
-        return action_result.set_status(phantom.APP_SUCCESS)
+        return self._action_result.set_status(phantom.APP_SUCCESS)
 
     def handle_action(self, param):
 
@@ -243,21 +260,58 @@ class CiscoThreatResponseConnector(BaseConnector):
         return response.status_code == 400 and response.json().get('error') \
                in ('invalid_client', 'wrong_client_creds')
 
+    def is_scope_missing(self, response):
+        return response.status_code == 403 and response.json().get("error") \
+               == "missing_scope"
+
+    def check_scopes(self, tr):
+        scope_checks = (
+            functools.partial(tr.inspect.inspect,
+                              {'content': 'testscope.com'}),
+            functools.partial(tr.enrich.deliberate.observables,
+                              [{"value": "testscope.com", "type": "domain"}])
+        )
+
+        all_missing_scopes = set()
+
+        for check in scope_checks:
+            try:
+                check()
+            except HTTPError as error:
+                self.debug_print(repr(error))
+
+                if self.is_scope_missing(error.response):
+                    all_missing_scopes.update(
+                        error.response.json().get('missing-scopes', set())
+                    )
+                else:
+                    raise error
+
+        if all_missing_scopes:
+            raise ScopeError(all_missing_scopes)
+
     def connect(self):
         message = None
 
         try:
-            return ThreatResponse(
+            tr = ThreatResponse(
                 client_id=self._client_id,
                 client_password=self._client_password,
                 region=self._region
             )
+
+            self.check_scopes(tr)
+            return tr
 
         except RegionError as error:
             self.debug_print(repr(error))
             message = (
                 'Please make sure that your Region is valid.'
             )
+
+        except ScopeError as error:
+            self.debug_print(repr(error))
+            message = error.message
 
         except HTTPError as error:
             self.debug_print(repr(error))
@@ -267,7 +321,15 @@ class CiscoThreatResponseConnector(BaseConnector):
                     '(Client ID and Client Password) are valid.'
                 )
             else:
-                message = 'Unexpected error.'
+                message = 'Unexpected HTTPError occurred.'
+
+        except ConnectionError as error:
+            self.debug_print(repr(error))
+            message = 'Failed to connect to Cisco Threat Response.'
+
+        except Exception as error:
+            self.debug_print(repr(error))
+            message = 'Unexpected error occurred.'
 
         raise Exception(message)
 
@@ -282,15 +344,14 @@ class CiscoThreatResponseConnector(BaseConnector):
 
         """
         # Access values in asset config by the name
-
         # Required values can be accessed directly
         required_config_name = config['required_config_name']
-
         # Optional values should use the .get() function
         optional_config_name = config.get('optional_config_name')
         """
 
         self._base_url = config.get('base_url')
+        self._action_result = self.add_action_result(ActionResult())
 
         self._client_id = config['client_id']
         self._client_password = config['client_password']
@@ -303,10 +364,12 @@ class CiscoThreatResponseConnector(BaseConnector):
         except Exception as error:
             if self.get_action_identifier() == 'test_connectivity':
                 self.save_progress(str(error))
-                self.set_status(phantom.APP_ERROR, "Test Connectivity Failed")
+                return self._action_result.set_status(
+                    phantom.APP_ERROR, "Test Connectivity Failed"
+                )
             else:
-                self.set_status(phantom.APP_ERROR, str(error))
-            return phantom.APP_ERROR
+                return self._action_result.set_status(phantom.APP_ERROR,
+                                                      str(error))
 
         return phantom.APP_SUCCESS
 
--- Apps/phciscothreatresponse/ciscothreatresponse_view.py
+++ Apps/phciscothreatresponse/ciscothreatresponse_view.py
@@ -47,3 +47,24 @@ def display_view(provides, all_app_runs, context):
             results.append(ctx_result)
 
     return "ciscothreatresponse_display_context.html"
+
+
+def display_view_verdict(provides, all_app_runs, context):
+    """ Function that displays view.
+
+    :param provides: action name
+    :param context: context
+    :param all_app_runs: all app runs
+    :return: html page
+    """
+
+    context['results'] = results = []
+    for summary, action_results in all_app_runs:
+        for result in action_results:
+
+            ctx_result = get_ctx_result(provides, result)
+            if not ctx_result:
+                continue
+            results.append(ctx_result)
+
+    return "ciscothreatresponse_display_verdict.html"
