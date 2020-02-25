56822564c0e3e9c5529b21a35451fd4565e46ad5
Alexandra Lomotan <alomotan@splunk.com>
2020-02-25 10:15:43 -0800

    fix connector name

--- Apps/phpassivetotal/passivetotal_connector.py
+++ Apps/phpassivetotal/passivetotal_connector.py
@@ -478,7 +478,7 @@ if __name__ == '__main__':
         in_json = json.loads(in_json)
         print(json.dumps(in_json, indent=4))
 
-        connector = OktaConnector()
+        connector = PassivetotalConnector()
         connector.print_progress_message = True
         ret_val = connector._handle_action(json.dumps(in_json), None)
 
