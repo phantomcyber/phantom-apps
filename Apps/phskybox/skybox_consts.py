90e6579e5657f559a98c8b3f7cb959a7a516e12a
mjiyani-crest <mjiyani@splunk.com>
2020-03-03 19:21:48 +0530

    Skybox: Initial code submission

---
+++ Apps/phskybox/skybox_consts.py
@@ -0,0 +1,19 @@
+# File: skybox_consts.py
+# Copyright (c) 2019 Splunk Inc.
+#
+# SPLUNK CONFIDENTIAL – Use or disclosure of this material in whole or in part
+# without a valid written license from Splunk Inc. is PROHIBITED.
+SKYBOX_WSDL = "{base_url}/skybox/webservice/jaxws/{service}?wsdl"
+
+SKYBOX_CONFIG_BASE_URL = "base_url"
+SKYBOX_CONFIG_USERNAME = "username"
+SKYBOX_CONFIG_PASSWORD = "password"
+
+SKYBOX_SERVICE_ADMINISTRATION = "administration"
+SKYBOX_SERVICE_FIREWALL = "firewall"
+SKYBOX_SERVICE_NETWORK = "network"
+SKYBOX_SERVICE_TICKETS = "tickets"
+SKYBOX_SERVICE_VULNERABILITIES = "vulnerabilities"
+
+SKYBOX_ROUTE_OUTPUT_TYPE_HTML = 0
+SKYBOX_ROUTE_OUTPUT_TYPE_XML = 1
