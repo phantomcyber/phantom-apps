c63a2eb510ed9348e3f634c5c0f6aae153c9137c
fp-user <fp-user@flashpoint.com>
2020-03-24 22:54:49 +0530

    FlashpointPhantomIntegration - Initial commit for the Flashpoint Phantom app

---
+++ Apps/phflashpoint/__init__.py
@@ -0,0 +1,13 @@
+# --
+# File: __init__.py
+#
+# Copyright (c) Flashpoint, 2020
+#
+# This unpublished material is proprietary to Flashpoint.
+# All rights reserved. The methods and
+# techniques described herein are considered trade secrets
+# and/or confidential. Reproduction or distribution, in whole
+# or in part, is forbidden except by express written permission
+# of Flashpoint.
+#
+# --
