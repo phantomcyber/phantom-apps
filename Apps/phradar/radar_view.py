90e73dd0f11cf35404951468c9a209643af554c0
Keenan Wells <keenan.wells@radarfirst.com>
2020-03-25 10:47:12 -0700

    Add separate views for action, change view logic. - View function returns context with action result parameters and status   along with data - Update template logic to render updated context result data. - Update get notes view to return 'no notes' message only for existing   incidents with no notes. - Update 'no notes' message.

--- Apps/phradar/radar_view.py
+++ Apps/phradar/radar_view.py
@@ -4,34 +4,35 @@
 # Licensed under Apache 2.0 (https://www.apache.org/licenses/LICENSE-2.0.txt)
 
 
-def display_create_incident_results(provides, all_app_runs, context):
-    context["create_incident_results"] = process_results(all_app_runs)
-    return "radar_display.html"
-
-
-def display_get_incident_results(provides, all_app_runs, context):
-    context["get_incident_results"] = process_results(all_app_runs)
-    return "radar_display.html"
-
-
-def display_add_note_results(provides, all_app_runs, context):
-    context["add_note_results"] = process_results(all_app_runs)
-    return "radar_display.html"
-
-
-def display_get_notes_results(provides, all_app_runs, context):
-    context["get_notes_results"] = process_results(all_app_runs)
-    if not len(context["get_notes_results"]):
-        context["get_note_results_none"] = True
-    return "radar_display.html"
-
-
-def process_results(all_app_runs):
-    data = []
+def display_view(provides, all_app_runs, context):
+    context["results"] = results = []
     for summary, action_results in all_app_runs:
-        for result in action_results:
-            # The phantom action result data is where the output of the action should be added. This always is a list, even when it contains only a single item
-            result_data = result.get_data()
-            if len(result_data) > 0:
-                data.append(result_data[0])
-    return data
+        for r in action_results:
+            result = process_results(r)
+            if not result:
+                continue
+            results.append(result)
+
+    if provides == "create privacy incident":
+        return_page = "radar_create_privacy_incident.html"
+    if provides == "get privacy incident":
+        return_page = "radar_get_privacy_incident.html"
+    if provides == "add note":
+        return_page = "radar_add_note.html"
+    if provides == "get notes":
+        return_page = "radar_get_notes.html"
+    return return_page
+
+
+def process_results(result):
+    result_data = {}
+    result_data["param"] = result.get_param()
+    result_data["status_success"] = result.get_status()
+    # The phantom action result data is where the output of the action should be added.
+    #  This always is a list, even when it contains only a single item
+    data = result.get_data()
+    if not data:
+        result_data["data"] = {}
+        return result_data
+    result_data["data"] = data[0]
+    return result_data
