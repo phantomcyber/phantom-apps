227d630239d72930ea2140c92a39b6749f5e716a
cpangam <cpangam@alumni.scu.edu>
2020-05-11 17:51:46 -0700

    Nextskope: Next to Master

--- Apps/phnetskope/netskope_view.py
+++ Apps/phnetskope/netskope_view.py
@@ -1,7 +1,8 @@
 # File: netskope_view.py
-# Copyright (c) 2018-2019 Splunk Inc.
+# Copyright (c) 2018-2020 Splunk Inc.
 #
 # Licensed under Apache 2.0 (https://www.apache.org/licenses/LICENSE-2.0.txt)
+
 from datetime import datetime
 
 
@@ -12,24 +13,18 @@ def get_ctx_result(provides, result):
     :param provides: action name
     :return: response data
     """
-
     ctx_result = {}
-
     param = result.get_param()
     summary = result.get_summary()
     data = result.get_data()
-
     ctx_result['param'] = param
-
     if summary:
         ctx_result['summary'] = summary
     ctx_result['action'] = provides
     if not data:
         ctx_result['data'] = {}
         return ctx_result
-
     ctx_result['data'] = _parse_data(data[0])
-
     return ctx_result
 
 
@@ -39,21 +34,17 @@ def _parse_data(data):
     :param data: response data
     :return: response data
     """
-
     for pages in data.get('page', []):
         try:
-            if pages.get("_insertion_epoch_timestamp"):
-                pages['_insertion_epoch_timestamp'] = '{}Z'.format(datetime.fromtimestamp(
-                    pages['_insertion_epoch_timestamp']).isoformat())
-
+            if pages.get('_insertion_epoch_timestamp'):
+                pages['_insertion_epoch_timestamp'] = ('{}Z').format(datetime.fromtimestamp(pages['_insertion_epoch_timestamp']).isoformat())
         except ValueError:
             pass
 
     for app in data.get('application', []):
         try:
-            if app.get("_insertion_epoch_timestamp"):
-                app['_insertion_epoch_timestamp'] = '{}Z'.format(datetime.fromtimestamp(
-                    app['_insertion_epoch_timestamp']).isoformat())
+            if app.get('_insertion_epoch_timestamp'):
+                app['_insertion_epoch_timestamp'] = ('{}Z').format(datetime.fromtimestamp(app['_insertion_epoch_timestamp']).isoformat())
         except ValueError:
             pass
 
@@ -68,16 +59,13 @@ def display_view(provides, all_app_runs, context):
     :param all_app_runs: all app runs
     :return: html page
     """
-
     context['results'] = results = []
     for summary, action_results in all_app_runs:
         for result in action_results:
-
             ctx_result = get_ctx_result(provides, result)
             if not ctx_result:
                 continue
             results.append(ctx_result)
 
-    return_page = "netskope_run_query.html"
-
+    return_page = 'netskope_run_query.html'
     return return_page
