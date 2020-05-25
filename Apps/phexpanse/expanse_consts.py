0e7a8d3647cca95ed29fe70f1b1706493fffbd61
mjiyani-crest <mjiyani@splunk.com>
2020-05-25 15:48:01 +0530

    Expanse: Updated license

--- Apps/phexpanse/expanse_consts.py
+++ Apps/phexpanse/expanse_consts.py
@@ -1,4 +1,8 @@
-# Expanse Consts
+# File: expanse_connector.py
+# Copyright (c) 2020 Splunk Inc.
+#
+# Licensed under Apache 2.0 (https://www.apache.org/licenses/LICENSE-2.0.txt)
+
 STATUS_CODE_200 = 200
 EXPANSE_USER_AGENT = "Expanse_Phantom/1.0.0"
 JSON_CONTENT_TYPE = "application/json"
