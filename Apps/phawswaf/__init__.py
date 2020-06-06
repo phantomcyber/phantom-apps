feacc857cf94c05d924dfc3485e336287619ae2a
cpangam <cpangam@alumni.scu.edu>
2020-06-05 17:16:02 -0700

    Merging repo phawswaf to github from source directory phawswaf

---
+++ Apps/phawswaf/__init__.py
@@ -0,0 +1,5 @@
+# File: __init__.py
+# Copyright (c) 2019 Splunk Inc.
+#
+# SPLUNK CONFIDENTIAL - Use or disclosure of this material in whole or in part
+# without a valid written license from Splunk Inc. is PROHIBITED.
