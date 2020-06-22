4569e7361ca1db17d5f8d891c34adff81c0a2a41
Drouin <Robert.Drouin@vantiv.com>
2020-06-22 17:56:46 -0400

    Initial commit.

---
+++ Apps/phfireeyeetp/fireeyeetp_consts.py
@@ -0,0 +1,5 @@
+# Define your constants here
+FIREEYEETP_US_BASE_PATH = "https://etp.us.fireeye.com/"
+FIREEYEETP_EU_BASE_PATH = "https://etp.eu.fireeye.com/"
+FIREEYEETP_AP_BASE_PATH = "https://etp.ap.fireeye.com/"
+FIREETEETP_LIST_ALERTS_ENDPOINT = "api/v1/alerts"
