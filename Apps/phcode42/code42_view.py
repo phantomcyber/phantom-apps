2378dc2a6e5f59c00de4769ac76a787c7a6fd2cb
Ricardo Munoz <rmunoz@rmunoz-mbp-9d822.sv.splunk.com>
2020-07-06 10:54:40 -0700

    adding phcode42 app

---
+++ Apps/phcode42/code42_view.py
@@ -0,0 +1,55 @@
+# File: code42_view.py
+# Copyright (c) 2018-2020 Splunk Inc.
+#
+# Licensed under Apache 2.0 (https://www.apache.org/licenses/LICENSE-2.0.txt)
+
+
+def get_ctx_result(provides, result):
+    """ Function that parses data.
+
+    :param result: result
+    :param provides: action name
+    :return: response data
+    """
+
+    ctx_result = {}
+
+    param = result.get_param()
+    summary = result.get_summary()
+    data = result.get_data()
+
+    ctx_result['param'] = param
+
+    if summary:
+        ctx_result['summary'] = summary
+    ctx_result['action'] = provides
+    if not data:
+        ctx_result['data'] = []
+        return ctx_result
+
+    ctx_result['data'] = data
+
+    return ctx_result
+
+
+def display_view(provides, all_app_runs, context):
+    """ Function that displays view.
+
+    :param provides: action name
+    :param context: context
+    :param all_app_runs: all app runs
+    :return: html page
+    """
+
+    context['results'] = results = []
+    for summary, action_results in all_app_runs:
+        for result in action_results:
+
+            ctx_result = get_ctx_result(provides, result)
+            if not ctx_result:
+                continue
+            results.append(ctx_result)
+
+    return_page = "code42_run_query.html"
+
+    return return_page
