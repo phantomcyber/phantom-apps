c2d19b19ddd815c2d37b03a68abf70a89fffed93
Ryan Means <rmeans@splunk.com>
2020-07-10 16:09:26 -0700

    PAPP-10382 python2 -> 3

--- Apps/phfortigate/fortigate_connector.py
+++ Apps/phfortigate/fortigate_connector.py
@@ -819,7 +819,7 @@ if __name__ == '__main__':
     with open(sys.argv[1]) as f:
         in_json = f.read()
         in_json = json.loads(in_json)
-        print(json.dumps(in_json, indent=4))
+        print((json.dumps(in_json, indent=4)))
         connector = FortiGateConnector()
         connector.print_progress_message = True
         connector._handle_action(json.dumps(in_json), None)
