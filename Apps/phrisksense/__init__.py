81805df7ac01aa13e7be69e976ced8a5203ce03c
Manasa Velladurai <manasa.velladurai@risksense.com>
2020-09-14 17:27:42 +0530

    RiskSense Phantom Integration - Initial App Submission

---
+++ Apps/phrisksense/__init__.py
@@ -0,0 +1,13 @@
+# --
+# File: __init_.py
+#
+# Copyright (c) RiskSense, 2020
+#
+# This unpublished material is proprietary to RiskSense.
+# All rights reserved. The methods and
+# techniques described herein are considered trade secrets
+# and/or confidential. Reproduction or distribution, in whole
+# or in part, is forbidden except by express written permission
+# of RiskSense.
+#
+# --
