cf0cc8e30c7b0e084b4c208a282bf3ae32c98389
Colin McKibben <colin.mckibben@fireeye.com>
2020-10-01 15:14:37 -0400

    Added Detection on Demand app to phantom apps folder.

---
+++ Apps/phdetectionondemand/detectionondemand_consts.py
@@ -0,0 +1,8 @@
+# Define your constants here
+DOD_API_AUTH_HEADER = 'feye-auth-key'
+
+DOD_HEALTH_ENDPOINT = '/health'
+DOD_FILES_ENDPOINT = '/files'
+DOD_HASHES_ENDPOINT = '/hashes'
+DOD_REPORTS_ENDPOINT = '/reports'
+DOD_PRESIGNED_URL_ENDPOINT = '/presigned-url'
