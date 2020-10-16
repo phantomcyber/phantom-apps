47f5dc6033515bed0935e2ebd74b40b7c4554e20
Kevin Lane <kevin.lane@axonius.com>
2020-10-16 13:15:26 -0400

    Renamed folder according to comment.

---
+++ Apps/phaxoniuscybersecurityassetmanagement/axoniuscybersecurityassetmanagement_consts.py
@@ -0,0 +1,5 @@
+# File: axoniuscybersecurityassetmanagement_consts.py
+# Copyright (c) 2019-2020 Splunk Inc.
+#
+# Licensed under Apache 2.0 (https://www.apache.org/licenses/LICENSE-2.0.txt)
+# Define your constants here
