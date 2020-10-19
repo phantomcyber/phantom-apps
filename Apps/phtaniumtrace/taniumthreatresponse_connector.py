50245768f808ce126fcf0d608b995f7b4853e988
Randy T Baldwin <baldwirt@sce.eix.com>
2020-10-19 09:57:46 -0400

    comma TokenError

--- Apps/phtaniumtrace/taniumthreatresponse_connector.py
+++ Apps/phtaniumtrace/taniumthreatresponse_connector.py
@@ -206,7 +206,7 @@ class TaniumThreatResponseConnector(BaseConnector):
             if phantom.is_fail(ret_val):
                 return action_result.get_status(), None
 
-        headers.update({'session': str(self._session_key),})
+        headers.update({'session': str(self._session_key)})
         if 'Content-Type' not in headers.keys():
             headers.update({'Content-Type': 'application/json'})
 
