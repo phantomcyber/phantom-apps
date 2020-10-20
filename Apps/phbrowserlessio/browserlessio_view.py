975941ce9ce3df56e7dc14c65e2c083bcf0f4858
Dhvani Vora <dhvani.vora@CDSYS.LOCAL>
2020-10-20 15:01:07 +0530

    Browserlessio(PAPP-11315) Initial code submission

---
+++ Apps/phbrowserlessio/browserlessio_view.py
@@ -0,0 +1,20 @@
+def get_ctx_result(result):
+    ctx_result = {}
+    summary = result.get_summary()
+    ctx_result['vault_id'] = summary.get('vault_id')
+    ctx_result['vault_file_name'] = summary.get('name')
+    ctx_result['vault_file_path'] = summary.get('vault_file_path')
+    ctx_result['message'] = result.get_message()
+    return ctx_result
+
+
+def display_screenshot(provides, all_app_runs, context):
+    context['results'] = results = []
+    for summary, action_results in all_app_runs:
+        for result in action_results:
+            ctx_result = get_ctx_result(result)
+            if not ctx_result:
+                continue
+            results.append(ctx_result)
+
+    return 'display_screenshot.html'
