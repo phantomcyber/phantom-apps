e58b4bd251f9b680e3e3d35bc67b4a091fc852ba
achandya <achandya@splunk.com>
2020-10-21 10:17:22 +0530

    Mimecast: Fixes for review suggestions

--- Apps/phmimecast/mimecast_consts.py
+++ Apps/phmimecast/mimecast_consts.py
@@ -7,5 +7,5 @@
 DEFAULT_MAX_RESULTS = 100
 MIMECAST_VALIDATE_INTEGER_MESSAGE = "Please provide a valid integer value in the {key} parameter"
 MIMECAST_ERROR_CODE_MESSAGE = "Error code unavailable"
-MIMECAST_ERROR_MESSAGE = "Unknown error occurred. Please check the asset configuration and|or action parameters."
-MIMECAST_UNICODE_DAMMIT_TYPE_ERROR_MESSAGE = "Error occurred while connecting to the Mimecast server. Please check the asset configuration and|or the action parameters."
+MIMECAST_ERROR_MESSAGE = "Unknown error occurred. Please check the asset configuration and|or action parameters"
+MIMECAST_UNICODE_DAMMIT_TYPE_ERROR_MESSAGE = "Error occurred while connecting to the Mimecast server. Please check the asset configuration and|or the action parameters"
