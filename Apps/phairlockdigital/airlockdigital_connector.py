8a21cfdf54bdd99284286d7fd5833f0b1b02b9e8
gusdadiya-crest <gusdadiya@splunk.com>
2020-11-03 10:40:09 +0530

    AirlockDigital: Added file copyright same as initial code

--- Apps/phairlockdigital/airlockdigital_connector.py
+++ Apps/phairlockdigital/airlockdigital_connector.py
@@ -1,4 +1,7 @@
 # File: airlockdigital_connector.py
+# Copyright (c) 2019-2020 Splunk Inc.
+#
+# Licensed under Apache 2.0 (https://www.apache.org/licenses/LICENSE-2.0.txt)
 
 # Phantom App imports
 import phantom.app as phantom
