ae697c37671afeaf9378a1b9bd435848d9303993
Ankit Mangal <ankit.mangal@CDSYS.LOCAL>
2020-11-06 11:44:33 +0530

    SMime(PAPP-9590): Minor changes in the constants name

--- Apps/phsmime/smime_consts.py
+++ Apps/phsmime/smime_consts.py
@@ -27,7 +27,7 @@ SMIME_DECRYPT_ERR2_MSG = "The received message was not encrypted"
 SMIME_DECRYPT_OK_MSG = "Email message decrypted successfully"
 
 # Error message handling constants
-ERROR_CODE_MSG = "Error code unavailable"
-ERROR_MSG_UNAVAILABLE = "Error message unavailable. Please check the asset configuration and|or action parameters"
+ERR_CODE_MSG = "Error code unavailable"
+ERR_MSG_UNAVAILABLE = "Error message unavailable. Please check the asset configuration and|or action parameters"
 PARSE_ERR_MSG = "Unable to parse the error message. Please check the asset configuration and|or action parameters"
 TYPE_ERR_MSG = "Error occurred while connecting to the SMime Server. Please check the asset configuration and|or the action parameters"
