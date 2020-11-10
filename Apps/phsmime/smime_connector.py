66e66e3db9aba57c4758ff60b82751d9ce57a8b4
Ankit Mangal <ankit.mangal@CDSYS.LOCAL>
2020-11-10 10:41:38 +0530

    SMime(PAPP-9590): Minor Code review changes

--- Apps/phsmime/smime_connector.py
+++ Apps/phsmime/smime_connector.py
@@ -469,7 +469,7 @@ if __name__ == '__main__':
     if (username and password):
         try:
             login_url = SmimeConnector._get_phantom_base_url() + '/login'
-            print ("Accessing the Login page")
+            print("Accessing the Login page")
             r = requests.get(login_url, verify=False)
             csrftoken = r.cookies['csrftoken']
 
@@ -482,12 +482,12 @@ if __name__ == '__main__':
             headers['Cookie'] = 'csrftoken=' + csrftoken
             headers['Referer'] = login_url
 
-            print ("Logging into Platform to get the session id")
+            print("Logging into Platform to get the session id")
             r2 = requests.post(login_url,
                                verify=False, data=data, headers=headers)
             session_id = r2.cookies['sessionid']
         except Exception as e:
-            print ("Unable to get session id from the platform. Error: " + str(e))
+            print("Unable to get session id from the platform. Error: " + str(e))
             exit(1)
 
     with open(args.input_test_json) as f:
@@ -503,6 +503,6 @@ if __name__ == '__main__':
             connector._set_csrf_info(csrftoken, headers['Referer'])
 
         ret_val = connector._handle_action(json.dumps(in_json), None)
-        print (json.dumps(json.loads(ret_val), indent=4))
+        print(json.dumps(json.loads(ret_val), indent=4))
 
     exit(0)
