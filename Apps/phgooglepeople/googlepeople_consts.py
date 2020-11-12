818348bf5018522c2af320e4877966c0a7f2d2e4
Ricardo Munoz <rmunoz@rmunoz-mbp-9d822.sv.splunk.com>
2020-11-12 09:58:19 -0800

    fixing copyrights

--- Apps/phgooglepeople/googlepeople_consts.py
+++ Apps/phgooglepeople/googlepeople_consts.py
@@ -1,3 +1,8 @@
+# File: googlepeople_consts.py
+# Copyright (c) 2020 Splunk Inc.
+#
+# Licensed under Apache 2.0 (https://www.apache.org/licenses/LICENSE-2.0.txt)
+
 GOOGLE_CONTACTS_SCOPE = 'https://www.googleapis.com/auth/contacts'
 GOOGLE_OTHER_CONTACTS_SCOPE_READ_ONLY = 'https://www.googleapis.com/auth/contacts.other.readonly'
 GOOGLE_DIRECTORY_SCPOPE_READ_ONLY = 'https://www.googleapis.com/auth/directory.readonly'
