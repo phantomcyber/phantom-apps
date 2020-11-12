53d59c3f368a90a45da73916d35077d2d18152d5
Ankit Mangal <ankit.mangal@CDSYS.LOCAL>
2020-11-12 11:19:20 +0530

    Hashicorp Vault(PAPP-10071): Minor changes in constants naming

--- Apps/phhashicorpvault/hashicorp_vault_consts.py
+++ Apps/phhashicorpvault/hashicorp_vault_consts.py
@@ -5,7 +5,7 @@ ACTION_ID_GET_SECRET = 'get_secret'
 ACTION_ID_LIST_SECRETS = 'list_secrets'
 
 # Error message handling constants
-ERROR_CODE_MSG = "Error code unavailable"
-ERROR_MSG_UNAVAILABLE = "Error message unavailable. Please check the asset configuration and|or action parameters"
+ERR_CODE_MSG = "Error code unavailable"
+ERR_MSG_UNAVAILABLE = "Error message unavailable. Please check the asset configuration and|or action parameters"
 PARSE_ERR_MSG = "Unable to parse the error message. Please check the asset configuration and|or action parameters"
 TYPE_ERR_MSG = "Error occurred while connecting to the Hashicorp Vault Server. Please check the asset configuration and|or the action parameters"
