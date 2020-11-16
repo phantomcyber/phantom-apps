2d3fd4e555e740e8e1d37fc9c91e3c3a13d9a1b4
Ronald Gil <rgil@splunk.com>
2020-11-16 14:01:14 -0800

    Fix compilation

--- Apps/phthirdpartytest/thirdpartytest_connector.py
+++ Apps/phthirdpartytest/thirdpartytest_connector.py
@@ -18,7 +18,6 @@ import requests
 import json
 from bs4 import BeautifulSoup
 
-# Test commit change
 
 class RetVal(tuple):
 
@@ -197,10 +196,10 @@ class ThirdpartytestConnector(BaseConnector):
         # Access action parameters passed in the 'param' dictionary
 
         # Required values can be accessed directly
-        reqparam1 = param['reqparam1']
+        #reqparam1 = param['reqparam1']
 
         # Optional values should use the .get() function
-        optparam1 = param.get('optparam1', '')
+        #optparam1 = param.get('optparam1', '')
 
         # make rest call
         ret_val, response = self._make_rest_call(
