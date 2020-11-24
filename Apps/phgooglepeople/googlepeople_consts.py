57c2b8b55c05da033bca82c487d368b5e5bb045c
gusdadiya-crest <gusdadiya@splunk.com>
2020-11-24 16:12:40 +0530

    Google People: Peer code review changes done

--- Apps/phgooglepeople/googlepeople_consts.py
+++ Apps/phgooglepeople/googlepeople_consts.py
@@ -5,7 +5,7 @@
 
 GOOGLE_CONTACTS_SCOPE = 'https://www.googleapis.com/auth/contacts'
 GOOGLE_OTHER_CONTACTS_SCOPE_READ_ONLY = 'https://www.googleapis.com/auth/contacts.other.readonly'
-GOOGLE_DIRECTORY_SCPOPE_READ_ONLY = 'https://www.googleapis.com/auth/directory.readonly'
+GOOGLE_DIRECTORY_SCOPE_READ_ONLY = 'https://www.googleapis.com/auth/directory.readonly'
 GOOGLE_PROFILE_SCOPE = 'https://www.googleapis.com/auth/userinfo.profile'
 OTHER_CONTACTS_RESOURCE_NAME_PREFIX = 'otherContacts/'
 
@@ -13,10 +13,11 @@ OTHER_CONTACTS_RESOURCE_NAME_PREFIX = 'otherContacts/'
 ERR_CODE_MSG = "Error code unavailable"
 ERR_MSG_UNAVAILABLE = "Error message unavailable. Please check the asset configuration and|or action parameters"
 PARSE_ERR_MSG = "Unable to parse the error message. Please check the asset configuration and|or action parameters"
-TYPE_ERR_MSG = "Error occurred while connecting to the Google People Server. Please check the asset configuration and|or the action parameters"
 
 # Constants relating to '_validate_integer'
 INVALID_INTEGER_ERR_MSG = "Please provide a valid integer value in the {}"
 INVALID_NON_NEGATIVE_INTEGER_ERR_MSG = "Please provide a valid non-negative integer value in the {}"
 
 PAGE_SIZE_KEY = "'page_size' action parameter"
+
+GOOGLE_FAILED_CREATE_CLIENT = "Failed to create the Google People client"
