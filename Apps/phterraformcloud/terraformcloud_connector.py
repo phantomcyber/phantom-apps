511baf4363c94fa5c36bc93e896f48cb29c9b257
haritashah_crest <harita.shah@crestdatasys.com>
2020-12-01 19:42:11 +0530

    Terraform(PAPP-9708): Removed extra parenthesis and added contains

--- Apps/phterraformcloud/terraformcloud_connector.py
+++ Apps/phterraformcloud/terraformcloud_connector.py
@@ -639,13 +639,13 @@ if __name__ == '__main__':
             r2 = requests.post(login_url, verify=False, data=data, headers=headers)
             session_id = r2.cookies['sessionid']
         except Exception as e:
-            print(("Unable to get session id from the platform. Error: " + str(e)))
+            print("Unable to get session id from the platform. Error: " + str(e))
             exit(1)
 
     with open(args.input_test_json) as f:
         in_json = f.read()
         in_json = json.loads(in_json)
-        print((json.dumps(in_json, indent=4)))
+        print(json.dumps(in_json, indent=4))
 
         connector = TerraformCloudConnector()
         connector.print_progress_message = True
@@ -655,6 +655,6 @@ if __name__ == '__main__':
             connector._set_csrf_info(csrftoken, headers['Referer'])
 
         ret_val = connector._handle_action(json.dumps(in_json), None)
-        print((json.dumps(json.loads(ret_val), indent=4)))
+        print(json.dumps(json.loads(ret_val), indent=4))
 
     exit(0)
