0bf56f543b344adbcdbfe26f3e28a3bb88f9109a
Ricardo Munoz <rmunoz@rmunoz-mbp-9d822.sv.splunk.com>
2021-01-06 13:31:47 -0800

    Adding Phishlabs App

---
+++ Apps/phphishlabs/phishlabs_view.py
@@ -0,0 +1,36 @@
+# File: phishlabs_view.py
+# Copyright (c) 2018-2021 Splunk Inc.
+#
+# Licensed under Apache 2.0 (https://www.apache.org/licenses/LICENSE-2.0.txt)
+
+
+def get_ctx_result(result):
+
+    ctx_result = {}
+    param = result.get_param()
+    summary = result.get_summary()
+    data = result.get_data()
+
+    ctx_result['param'] = param
+
+    if (data):
+        ctx_result['data'] = data
+
+    if (summary):
+        ctx_result['summary'] = summary
+
+    return ctx_result
+
+
+def display_config(provides, all_app_runs, context):
+
+    context['results'] = results = []
+    for summary, action_results in all_app_runs:
+        for result in action_results:
+
+            ctx_result = get_ctx_result(result)
+            if (not ctx_result):
+                continue
+            results.append(ctx_result)
+    # print context
+    return 'display_config.html'
