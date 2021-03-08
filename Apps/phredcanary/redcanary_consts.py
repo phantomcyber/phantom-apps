eec37bad061b76b4920b72b566043263e2b4f275
Matt Hastings <>
2021-03-08 16:30:18 -0500

    Phredcanary app initial commit

---
+++ Apps/phredcanary/redcanary_consts.py
@@ -0,0 +1,11 @@
+# Define your constants here
+
+RC_DATE_TIME_FORMAT = "%Y-%m-%dT%H:%M:%SZ"
+MAX_PER_PAGE = 50
+STR_LAST_RUN = "last_run"
+STR_META = "meta"
+STR_TOTAL_ITEMS = "total_items"
+STR_NOT_FOUND = "not_found"
+STR_PER_PAGE = "per_page="
+STR_SINCE = "since="
+STR_PAGE = "page="
