06b5243c9a92c7f51afc189feabcd6dc4d72c3cd
kgoyal <kgoyal@splunk.com>
2021-03-11 15:48:44 +0530

    arcsight community initial commit

---
+++ Apps/arcsight/__init__.py
@@ -0,0 +1,10 @@
+# --
+# File: __init__.py
+#
+# Copyright (c) 2017-2021 Splunk Inc.
+#
+# SPLUNK CONFIDENTIAL - Use or disclosure of this material in whole or in part
+# without a valid written license from Splunk Inc. is PROHIBITED.
+#
+#
+# --
