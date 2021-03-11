876c42a4ae6b0a8c776a257171cc0cb0ce616089
kgoyal <kgoyal@splunk.com>
2021-03-11 16:23:11 +0530

    copyright update

--- Apps/pharcsight/arcsight_consts.py
+++ Apps/pharcsight/arcsight_consts.py
@@ -1,12 +1,8 @@
-# --
 # File: arcsight_consts.py
+# Copyright (c) 2021 Splunk Inc.
 #
-# Copyright (c) 2017-2021 Splunk Inc.
+# Licensed under Apache 2.0 (https://www.apache.org/licenses/LICENSE-2.0.txt)
 #
-# SPLUNK CONFIDENTIAL - Use or disclosure of this material in whole or in part
-# without a valid written license from Splunk Inc. is PROHIBITED.
-#
-# --
 
 # Json keys
 ARCSIGHT_JSON_BASE_URL = "base_url"
