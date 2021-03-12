d07781bd1feab0f74fb7252bd9b51408b23e2ef2
rsojitra <rsojitra@splunk.com>
2021-03-12 17:09:20 +0530

    PAPP-13776 Change copyright year to 2021 only as this is a new app.

--- Apps/phawswafv2/awswafv2_connector.py
+++ Apps/phawswafv2/awswafv2_connector.py
@@ -1,5 +1,5 @@
-# File: awswaf_connector.py
-# Copyright (c) 2019-2021 Splunk Inc.
+# File: awswafv2_connector.py
+# Copyright (c) 2021 Splunk Inc.
 #
 # SPLUNK CONFIDENTIAL - Use or disclosure of this material in whole or in part
 # without a valid written license from Splunk Inc. is PROHIBITED.
