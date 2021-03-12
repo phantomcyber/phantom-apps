f5c07169201b2f980486a639291e8fe2a56fce1a
rsojitra <rsojitra@splunk.com>
2021-03-12 17:50:06 +0530

    PAPP-13776 Updating opensource license in all file.

--- Apps/phawswafv2/awswafv2_connector.py
+++ Apps/phawswafv2/awswafv2_connector.py
@@ -1,8 +1,8 @@
 # File: awswafv2_connector.py
 # Copyright (c) 2021 Splunk Inc.
 #
-# SPLUNK CONFIDENTIAL - Use or disclosure of this material in whole or in part
-# without a valid written license from Splunk Inc. is PROHIBITED.
+# Licensed under Apache 2.0 (https://www.apache.org/licenses/LICENSE-2.0.txt)
+#
 
 import ipaddress
 import json
--- Apps/phawswafv2/awswafv2_consts.py
+++ Apps/phawswafv2/awswafv2_consts.py
@@ -1,8 +1,8 @@
 # File: awswafv2_consts.py
 # Copyright (c) 2021 Splunk Inc.
 #
-# SPLUNK CONFIDENTIAL - Use or disclosure of this material in whole or in part
-# without a valid written license from Splunk Inc. is PROHIBITED.
+# Licensed under Apache 2.0 (https://www.apache.org/licenses/LICENSE-2.0.txt)
+#
 
 AWSWAF_VERSION_V2 = 'wafv2'
 AWSWAF_ACCESS_KEY = 'access_key_id'
