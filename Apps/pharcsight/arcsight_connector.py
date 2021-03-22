461a59a58dbb38da972fd4a21bd66605cd709b18
kgoyal <kgoyal@splunk.com>
2021-03-22 15:31:23 +0530

    arcsight year update

--- Apps/pharcsight/arcsight_connector.py
+++ Apps/pharcsight/arcsight_connector.py
@@ -1,5 +1,5 @@
 # File: arcsight_connector.py
-# Copyright (c) 2021 Splunk Inc.
+# Copyright (c) 2017-2021 Splunk Inc.
 #
 # Licensed under Apache 2.0 (https://www.apache.org/licenses/LICENSE-2.0.txt)
 #
