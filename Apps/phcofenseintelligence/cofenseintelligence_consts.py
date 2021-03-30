c4b47f36c36dcd5aa8e00361f038c4926004e58d
kgoyal <kgoyal@splunk.com>
2021-03-30 18:59:33 +0530

    copyright update

--- Apps/phcofenseintelligence/cofenseintelligence_consts.py
+++ Apps/phcofenseintelligence/cofenseintelligence_consts.py
@@ -1,8 +1,8 @@
 # File: cofenseintelligence_consts.py
 # Copyright (c) 2020-2021 Splunk Inc.
 #
-# SPLUNK CONFIDENTIAL - Use or disclosure of this material in whole or in part
-# without a valid written license from Splunk Inc. is PROHIBITED.
+# Licensed under Apache 2.0 (https://www.apache.org/licenses/LICENSE-2.0.txt)
+#
 
 PHISHME_API_SEARCH = "https://www.threathq.com/apiv1"
 PHISHME_API_THREAT_UPDATE = "/threat/updates"
