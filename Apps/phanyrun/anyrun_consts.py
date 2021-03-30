a006b2c38565de34bbb7ec50b46a03dae8fec205
Erica Pescio <edro15@users.noreply.github.com>
2021-03-30 17:41:50 +0200

    Initial upload of files

---
+++ Apps/phanyrun/anyrun_consts.py
@@ -0,0 +1,10 @@
+# File: anyrun_consts.py
+#
+# Copyright (c) 2021 Splunk Inc.
+#
+# Licensed under Apache 2.0 (https://www.apache.org/licenses/LICENSE-2.0.txt)
+
+# API Endpoints
+ANYRUN_TEST_CONNECTIVITY_ENDPOINT = 'environment'
+ANYRUN_DETONATE_FILE_ENDPOINT = 'analysis'
+ANYRUN_GET_REPORT_ENDPOINT = 'analysis/{taskid}'
