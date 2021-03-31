ffd3a4c9d45dd4793e1cbb580d14fc011c4d0325
arathore <arathore@splunk.com>
2021-03-31 13:18:06 +0530

    Redmine: Updated copyrights

--- Apps/phredmine/redmine_consts.py
+++ Apps/phredmine/redmine_consts.py
@@ -1,4 +1,8 @@
-# Define your constants here
+# File: redmine_consts.py
+#
+# Copyright (c) 2021 Splunk Inc.
+#
+# Licensed under Apache 2.0 (https://www.apache.org/licenses/LICENSE-2.0.txt)
 
 REDMINE_BACKFILL_DAYS = 7
 REDMINE_TICKET_ARTIFACT_LABEL = "redmine_ticket"
