323d2ef06f76013e201d1f4621629dd8c7055ce2
kgoyal <kgoyal@splunk.com>
2021-04-01 17:44:19 +0530

    copyright update

--- Apps/phrss/parser_helper.py
+++ Apps/phrss/parser_helper.py
@@ -1,5 +1,5 @@
 # File: parser_helper.py
-# Copyright (c) 2021 Splunk Inc.
+# Copyright (c) 2017-2021 Splunk Inc.
 #
 # SPLUNK CONFIDENTIAL - Use or disclosure of this material in whole or in part
 # without a valid written license from Splunk Inc. is PROHIBITED.
