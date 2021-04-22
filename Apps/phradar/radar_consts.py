be7eb15f669d4147eef4df6f48fc9d6b357e875f
Ankit Mangal <ankit.mangal@CDSYS.LOCAL>
2021-04-22 12:08:51 +0530

    Updated the license

--- Apps/phradar/radar_consts.py
+++ Apps/phradar/radar_consts.py
@@ -1,5 +1,4 @@
 # File: radar_consts.py
-# Copyright (c) 2020-2021 Splunk Inc.
 #
 # Licensed under Apache 2.0 (https://www.apache.org/licenses/LICENSE-2.0.txt)
 ALLOW_SELF_SIGNED_CERTS = "ALLOW_SELF_SIGNED_CERTS"
--- Apps/phradar/radar_view.py
+++ Apps/phradar/radar_view.py
@@ -1,5 +1,4 @@
 # File: radar_view.py
-# Copyright (c) 2020-2021 Splunk Inc.
 #
 # Licensed under Apache 2.0 (https://www.apache.org/licenses/LICENSE-2.0.txt)
 
