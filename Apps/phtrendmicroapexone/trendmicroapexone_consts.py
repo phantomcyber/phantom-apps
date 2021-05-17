6dccf172c7fbc1cba9f4c39b742ec0da272b7ce9
Daniel Federschmidt <dfederschmidt@splunk.com>
2021-05-17 14:18:32 +0200

    New App: Trend Micro Apex One (#575)
    * initial version of trend micro apex one app for phantom
    
    * minor adjustments
    
    * formatting
    
    * formatting.2
    
    * formatting.3 (newline)
    
    * Developer checklist changes and bug fix for PAPP-16653, PAPP-16654, PAPP-16655, PAPP-16656
    
    * Peer review changes
    
    * Update Apps/phtrendmicroapexone/trendmicroapexone.json
    
    Updating the minimal datapaths
    
    * Update trendmicroapexone_consts.py
    
    Removing unused constant
    
    Co-authored-by: Ankit Mangal <ankit.mangal@CDSYS.LOCAL>
    Co-authored-by: Mit Jiyani <57654567+mjiyani-crest@users.noreply.github.com>

---
+++ Apps/phtrendmicroapexone/trendmicroapexone_consts.py
@@ -0,0 +1,13 @@
+# File: trendmicroapexone_consts.py
+#
+# Copyright (c) 2021 Splunk Inc.
+#
+# Licensed under Apache 2.0 (https://www.apache.org/licenses/LICENSE-2.0.txt)
+
+APEX_ONE_PRODUCT_AGENTS_ENDPOINT = "/WebApp/API/AgentResource/ProductAgents"
+
+APEX_ONE_RESPONSE_EMPTY_MSG = "Empty result set from server. Please check your inputs"
+APEX_ONE_ERR_CODE_MSG = "Error code unavailable"
+APEX_ONE_ERR_MSG_UNAVAILABLE = "Error message unavailable. Please check the asset configuration and|or action parameters"
+APEX_ONE_PARSE_ERR_MSG = "Unable to parse the error message. Please check the asset configuration and|or action parameters"
+APEX_ONE_ERR_SERVER_RES = "Cannot process server response"
