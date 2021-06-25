265b152ecf473bf0d0cd0e64f48f99dadee0d5c9
Kijtapart Nawattranakul <knawattranakul@splunk.com>
2021-06-25 15:11:49 -0700

    fix wrong copyright

--- Apps/phvxstream/__init__.py
+++ Apps/phvxstream/__init__.py
@@ -1,8 +1,7 @@
 # File: __init__.py
 # Copyright (c) 2021 Splunk Inc.
 #
-# SPLUNK CONFIDENTIAL - Use or disclosure of this material in whole or in part
-# without a valid written license from Splunk Inc. is PROHIBITED.# init.py
+# Licensed under Apache 2.0 (https://www.apache.org/licenses/LICENSE-2.0.txt)# init.py
 #
 # Copyright (C) 2018 Hybrid Analysis GmbH
 #
