9df8aee741c09ffe38bab2629d3abef422591c5b
crestdatasystems <it.systems@crestdatasys.com>
2021-07-02 18:57:15 +0530

    Agari: Initial app submission

---
+++ Apps/phagari/__init__.py
@@ -0,0 +1,12 @@
+# File: __init__.py
+#
+# Copyright (c) Agari, 2021
+#
+# This unpublished material is proprietary to Agari.
+# All rights reserved. The methods and
+# techniques described herein are considered trade secrets
+# and/or confidential. Reproduction or distribution, in whole
+# or in part, is forbidden except by express written permission
+# of Agari.
+#
+# Licensed under Apache 2.0 (https://www.apache.org/licenses/LICENSE-2.0.txt)
