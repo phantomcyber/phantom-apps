c864d3c6eccb892ab9135e0631dd3aae042b0a47
jgagan-crest <jgagan@splunk.com>
2021-08-03 19:05:18 +0530

    Adding a watson app with V3 version

---
+++ Apps/phibmwatsonv3/watsonv3_consts.py
@@ -0,0 +1,7 @@
+# File: watsonv3_consts.py
+# Copyright (c) 2021 Splunk Inc.
+#
+# Licensed under Apache 2.0 (https://www.apache.org/licenses/LICENSE-2.0.txt)
+#
+
+GET_DEFAULT_VERSION = '2018-05-01'
