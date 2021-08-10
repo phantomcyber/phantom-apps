870ea1729de7cda917722b6dac105b15d601c571
Kijtapart Nawattranakul <knawattranakul@splunk.com>
2021-08-09 21:45:16 -0700

    Feat: Add consts for blockUrls.

--- Apps/phpanorama/panorama_consts.py
+++ Apps/phpanorama/panorama_consts.py
@@ -88,6 +88,9 @@ APP_GRP_SEC_POL_ELEM = "<application><member>{app_group_name}</member></applicat
 
 URL_PROF_XPATH = "{config_xpath}/profiles/url-filtering/entry[@name='{url_profile_name}']"
 URL_PROF_ELEM = "<description>Created by Phantom for Panorama</description><action>block</action><block-list><member>{url}</member></block-list>"
+URL_PROF_ELEM_9 = "<credential-enforcement><mode><disabled/></mode><block><member>{url_category_name}</member></block></credential-enforcement><block><member>{url_category_name}</member></block>"
+URL_CATEGORY_XPATH = "{config_xpath}/profiles/custom-url-category/entry[@name='{url_profile_name}']"
+URL_CATEGORY_ELEM = "<description>Created by Phantom for Panorama</description><list><member>{url}</member></list><type>URL List</type>"
 DEL_URL_XPATH = "/block-list/member[text()='{url}']"
 
 APP_GRP_XPATH = "{config_xpath}/application-group/entry[@name='{app_group_name}']"
