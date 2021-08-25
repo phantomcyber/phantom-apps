4d1850ffaff8b3a5ceb65cf6574b548f9ca13465
abhijeeta-metron <akamble@metronlabs.com>
2021-08-25 11:46:26 +0530

    implemented fetch_files action

--- Apps/phsentinelone/sentinelone_connector.py
+++ Apps/phsentinelone/sentinelone_connector.py
@@ -481,6 +481,43 @@ class SentineloneConnector(BaseConnector):
                 return action_result.get_status()
         return action_result.set_status(phantom.APP_SUCCESS)
 
+    def _handle_fetch_files(self, param):
+        self.save_progress("In action handler for: {0}".format(self.get_action_identifier()))
+        action_result = self.add_action_result(ActionResult(dict(param)))
+        ip_hostname = param['ip_hostname']
+        file_path = param['file_path']
+        password = param["password"]
+        try:
+            ret_val = self._get_agent_id(ip_hostname, action_result)
+        except Exception:
+            return action_result.set_status(phantom.APP_ERROR, "Did not get proper response from the server")
+        self.save_progress('Agent query: {}'.format(ret_val))
+        if ret_val == '0':
+            return action_result.set_status(phantom.APP_ERROR, "Endpoint not found")
+        elif ret_val == '99':
+            return action_result.set_status(phantom.APP_ERROR, "More than one endpoint found")
+        else:
+            summary = action_result.update_summary({})
+            summary['ip_hostname'] = ip_hostname
+            summary['file_path'] = file_path
+            summary['password'] = password
+            summary['agent_id'] = ret_val
+            header = self.HEADER
+            header["Authorization"] = "APIToken %s" % self.token
+            body = {
+                "data": {
+                    "files": [file_path],
+                    "password": password
+                }
+            }
+            ret_val, response = self._make_rest_call('/web/api/v2.1/agents/{}/actions/fetch-files'.format(ret_val),
+                action_result, headers=header, data=json.dumps(body), method='post')
+            self.save_progress("Ret_val: {0}".format(ret_val))
+            if phantom.is_fail(ret_val):
+                self.save_progress("Failed to scan endpoint. Error: {0}".format(action_result.get_message()))
+                return action_result.get_status()
+        return action_result.set_status(phantom.APP_SUCCESS)
+
     def _handle_get_endpoint_info(self, param):
         self.save_progress("In action handler for: {0}".format(self.get_action_identifier()))
         action_result = self.add_action_result(ActionResult(dict(param)))
@@ -685,6 +722,8 @@ class SentineloneConnector(BaseConnector):
             ret_val = self._handle_shutdown_endpoints(param)
         elif action_id == 'broadcast_message':
             ret_val = self._handle_broadcast_message(param)
+        elif action_id == 'fetch_files':
+            ret_val = self._handle_fetch_files(param)
         elif action_id == 'scan_endpoint':
             ret_val = self._handle_scan_endpoint(param)
         elif action_id == 'get_endpoint_info':
