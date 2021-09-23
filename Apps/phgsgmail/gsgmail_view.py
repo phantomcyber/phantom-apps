3ab4e2b64287fa19b9e07ddfd727215cb729841c
cpangam <cpangam@splunk.com>
2021-09-23 16:18:04 -0700

    PAPP-19724 GS Gmail: Next to Master

--- Apps/phgsgmail/gsgmail_view.py
+++ Apps/phgsgmail/gsgmail_view.py
@@ -37,3 +37,17 @@ def list_users(provides, all_app_runs, context):
             results.append(ctx_result)
 
     return 'gsgmail_list_users.html'
+
+
+def get_email(provides, all_app_runs, context):
+
+    context['results'] = results = []
+
+    for summary, action_results in all_app_runs:
+        for result in action_results:
+            ctx_result = _get_ctx_result(result, provides)
+            if not ctx_result:
+                continue
+            results.append(ctx_result)
+
+    return 'gsgmail_get_email.html'
