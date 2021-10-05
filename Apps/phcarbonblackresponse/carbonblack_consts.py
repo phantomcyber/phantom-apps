8862955a7a14dfd20ef227aa608523a3a48d46c5
Tony Cihak <tcihak@splunk.com>
2021-10-05 08:54:28 -0700

    PAPP-9168 removed unused variable

--- Apps/phcarbonblackresponse/carbonblack_consts.py
+++ Apps/phcarbonblackresponse/carbonblack_consts.py
@@ -82,5 +82,4 @@ CARBONBLACK_ERR_MSG = "Unknown error occurred. Please check the asset configurat
 CARBONBLACK_UNICODE_DAMMIT_TYPE_ERR_MSG = "Error occurred while connecting to the Carbon Black Response server."
 CARBONBLACK_UNICODE_DAMMIT_TYPE_ERR_MSG += " Please check the asset configuration and|or the action parameters."
 CARBONBLACK_ERR_UPDATE_ALERTS_PARAM_IDS = "Either 'query' or 'alert_ids' parameters are required for this action."
-CARBONBLACK_ERR_UPDATE_ALERTS_PARAM_ASSIGNED_TO = "If 'assigned_to' parameter is set, then 'requested status' is required."
 CARBONBLACK_GROUP_ID_MSG = "Group ID unavailable"
