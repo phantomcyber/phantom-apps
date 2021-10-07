50de7bb8d99507c3e4abca06aee3d0ded945bdc0
arathore <arathore@splunk.com>
2021-10-07 12:14:32 +0530

    ThreatQ: Removed .gitignore and README.md file

--- Apps/phthreatquotient/compile.sh
+++ Apps/phthreatquotient/compile.sh
@@ -1,4 +1,19 @@
 #!/bin/bash
+###########################################################################################################
+# File: compile.sh
+#
+# ThreatQuotient Proprietary and Confidential
+# Copyright (c)2021 ThreatQuotient, Inc. All rights reserved.
+#
+# NOTICE: All information contained herein, is, and remains the property of ThreatQuotient, Inc.
+# The intellectual and technical concepts contained herein are proprietary to ThreatQuotient, Inc.
+# and its suppliers and may be covered by U.S. and Foreign Patents, patents in process, and are
+# protected by trade secret or copyright law.
+#
+# Dissemination of this information or reproduction of this material is strictly forbidden unless prior
+# written permission is obtained from ThreatQuotient, Inc.
+###########################################################################################################
+
 # Check JSON first!
 if $(cat threatq_app/threatq.json | jq . > /dev/null); then
     for f in threatq_app/*.py threatq_app/**/*.py;
