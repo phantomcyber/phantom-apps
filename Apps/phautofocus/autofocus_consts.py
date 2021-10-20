48a829e3e95934c9cbc85e845c5375ae41f9964e
gusdadiya-crest <72184188+gusdadiya-crest@users.noreply.github.com>
2021-10-20 15:19:09 +0530

    PAPP-19898: AutoFocus: Moved under non-certified app (#734)
    * Added lib folder
    
    * Added exception handling
    
    * Peer review changes completed
    
    * Added unquote
    
    * Added pipeline failure in expected_failures file
    
    * removed key from expected_failures
    
    Co-authored-by: Mit Jiyani <mjiyani@splunk.com>

--- Apps/phautofocus/autofocus_consts.py
+++ Apps/phautofocus/autofocus_consts.py
@@ -15,3 +15,11 @@ AF_JSON_TAG = "tag"
 
 AF_ERR_INVALID_SCOPE = "Invalid scope: {0}"
 AF_ERR_TOO_BIG = "from + size can not be greater than {0}"
+
+# Constants relating to '_get_error_message_from_exception'
+ERR_CODE_MSG = "Error code unavailable"
+ERR_MSG_UNAVAILABLE = "Error message unavailable. Please check the asset configuration and|or action parameters"
+PARSE_ERR_MSG = "Unable to parse the error message. Please check the asset configuration and|or action parameters"
+TYPE_ERR_MSG = "Error occurred while connecting to the Auto Focus Server. Please check the asset configuration and|or the action parameters"
+
+STATE_FILE_CORRUPT_ERR = "Error occurred while loading the state file due to its unexpected format. Resetting the state file with the default format. Please try again"
